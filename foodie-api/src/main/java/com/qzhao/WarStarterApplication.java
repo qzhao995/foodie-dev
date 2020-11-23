4ca8b1c648a8e6ea44ae7bbdcfe5b496263ce121
qzhao995 <17809268287@163.com>
2020-11-23 16:06:31 +0800

    up war

---
+++ foodie-api/src/main/java/com/qzhao/WarStarterApplication.java
@@ -0,0 +1,14 @@
+package com.qzhao;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+// 打包war [4] 增加war的启动类
+public class WarStarterApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 指向FoodieApplication这个springboot启动类
+        return builder.sources(FoodieApplication.class);
+    }
+}
